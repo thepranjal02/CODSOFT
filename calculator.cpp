72ac0b3fa0981a44d13580eab225229da3cf926b
Pranjal <92870736+thepranjal02@users.noreply.github.com>
2023-08-25 15:45:32 +0530

    Codsoft Task 2: Simple calculator

---
+++ calculator.cpp
@@ -0,0 +1,29 @@
+# include <iostream>
+using namespace std;
+int main() {
+  char op;
+  float num1, num2;
+  cout << "Enter operator: +, -, *, /: "<<endl;
+  cin >> op;
+  cout << "Enter two operands: "<<endl;
+  cin >> num1 >> num2;
+  switch(op) {
+    case '+':
+      cout << num1 << " + " << num2 << " = " << num1 + num2<<endl;
+      break;
+    case '-':
+      cout << num1 << " - " << num2 << " = " << num1 - num2<<endl;
+      break;
+    case '*':
+      cout << num1 << " * " << num2 << " = " << num1 * num2<<endl;
+      break;
+    case '/':
+      cout << num1 << " / " << num2 << " = " << num1 / num2<<endl;
+      break;
+    default:
+      cout << "Error! operator is not correct"<<endl;
+      break;
+  }
+  return 0;
+}
+
